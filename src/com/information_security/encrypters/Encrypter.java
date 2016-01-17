82122a7ca15a4abc339fd1cbb595ae65e04581f1
Artyom Kakayev <artyom25kakayev@mail.ru>
2016-01-17 17:49:50 +0600

    + interface for encrypters created. + base GUI created

---
+++ src/com/information_security/encrypters/Encrypter.java
@@ -0,0 +1,8 @@
+package com.information_security.encrypters;
+
+public interface Encrypter {
+
+	public String encrypt(String input);
+	
+	public String decrypt(String input);
+}
---
+++ src/com/information_security/gui/MainFrame.java
@@ -0,0 +1,111 @@
+package com.information_security.gui;
+
+import javax.swing.ButtonGroup;
+
+public class MainFrame extends javax.swing.JFrame {
+
+    public MainFrame() {
+        initComponents();
+    }
+    
+    private void initComponents() {
+
+        jRadioButton1 = new javax.swing.JRadioButton();
+        jRadioButton2 = new javax.swing.JRadioButton();
+        jButton1 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jRadioButton1.setText("метод подстановок");
+        jRadioButton1.setActionCommand("1");
+        jRadioButton2.setText(" с помощью датчика ПСЧ");
+        jRadioButton2.setActionCommand("2");
+        jRadioButton1.setSelected(true);
+        group = new ButtonGroup();
+        group.add(jRadioButton1);
+        group.add(jRadioButton2);
+
+        jButton1.setText("Выбрать");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(72, 72, 72)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jRadioButton1)
+                            .addComponent(jRadioButton2)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(105, 105, 105)
+                        .addComponent(jButton1)))
+                .addContainerGap(145, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addComponent(jRadioButton1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jRadioButton2)
+                .addGap(18, 18, 18)
+                .addComponent(jButton1)
+                .addContainerGap(34, Short.MAX_VALUE))
+        );
+
+        pack();
+    }                   
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        switch (group.getSelection().getActionCommand()) {
+		case "1":
+			SubstitutionEncoderFrame substitutionEncoderFrame= new SubstitutionEncoderFrame();
+			substitutionEncoderFrame.open();
+			break;
+		case "2":
+			
+			break;
+		default:
+			break;
+		}
+    }                                        
+
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+      
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainFrame().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify
+    private ButtonGroup group;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JRadioButton jRadioButton1;
+    private javax.swing.JRadioButton jRadioButton2;
+    // End of variables declaration                   
+}
